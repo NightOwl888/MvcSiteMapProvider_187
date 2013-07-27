b2584a73652d2f6ca70848e0d70c535153177c71
Shad Storhaug <shad@shadstorhaug.com>
2013-07-27 20:29:52 +0700

    Created project to demonstrate usage of AllowAnonymous attribute.

---
+++ MvcSiteMapProvider_187/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _187_AuthorizeAttribute
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MvcSiteMapProvider_187/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_187_AuthorizeAttribute.MvcApplication" Language="C#" %>
